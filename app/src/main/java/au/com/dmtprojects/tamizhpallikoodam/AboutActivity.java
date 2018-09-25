6627704fc24433bf1167e071f1a93751589eff7e
Dirk Arends <dirk@arends.com.au>
2018-09-26 01:02:34 +1000

    Changed Package name from au.com.codycodes.tpk.tamizhpallikoodam to au.com.dmtprojects.tamizhpallikoodam

---
+++ app/src/main/java/au/com/dmtprojects/tamizhpallikoodam/AboutActivity.java
@@ -0,0 +1,18 @@
+package au.com.dmtprojects.tamizhpallikoodam;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import tamizhpallikoodam.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+    }
+}
+
